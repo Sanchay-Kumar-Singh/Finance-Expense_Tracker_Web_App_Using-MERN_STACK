186608de5d5e667af9e1e7679f284551f04ba5bf
Sanchay-Kumar-Singh <sanchaysingh62425@gmail.com>
2026-02-18 20:54:09 +0530

    update live project

--- server/config/db.js
+++ server/config/db.js
@@ -1,36 +1,36 @@
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-
-    console.log(`MongoDB connected ✅ Host: ${conn.connection.host}`);
-  } catch (err) {
-    console.error("MongoDB connection error❌ :", err.message);
-    // process.exit(1);
-  }
-};
-
-module.exports = connectDB;
 // const mongoose = require("mongoose");
 
-// let isConnected = false;
-
 // const connectDB = async () => {
-//   if (isConnected) {
-//     return;
-//   }
-
 //   try {
 //     const conn = await mongoose.connect(process.env.MONGO_URI);
 
-//     isConnected = conn.connections[0].readyState;
 //     console.log(`MongoDB connected ✅ Host: ${conn.connection.host}`);
 //   } catch (err) {
-//     console.error("MongoDB connection error ❌:", err.message);
-//     throw err; // do NOT use process.exit in serverless
+//     console.error("MongoDB connection error❌ :", err.message);
+//     process.exit(1);
 //   }
 // };
 
 // module.exports = connectDB;
+const mongoose = require("mongoose");
+
+let isConnected = false;
+
+const connectDB = async () => {
+  if (isConnected) {
+    return;
+  }
+
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    isConnected = conn.connections[0].readyState;
+    console.log(`MongoDB connected ✅ Host: ${conn.connection.host}`);
+  } catch (err) {
+    console.error("MongoDB connection error ❌:", err.message);
+    throw err; // do NOT use process.exit in serverless
+  }
+};
+
+module.exports = connectDB;
 
